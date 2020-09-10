cd883754b29fad97df298d40379c7c17907cf2ff
serpest <serpest@protonmail.com>
2020-09-10 17:55:16 +0200

    Move properties logic from MainApp to MainAppController

--- src/main/java/com/serpest/rebuk/MainApp.java
+++ src/main/java/com/serpest/rebuk/MainApp.java
@@ -1,10 +1,5 @@
 package com.serpest.rebuk;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 import com.serpest.rebuk.controller.MainAppController;
 import com.serpest.rebuk.load.ViewFactory;
 
@@ -14,25 +9,8 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-	private static final String PROPERTIES_FILE = "data" + File.separator + "Rebuk_options.properties";
-
-	public static Properties options;
-
-	static {
-		try {
-			options = new Properties();
-			loadProperties();
-		} catch (IOException exc) {}
-	}
-
 	public static void main(String[] args) {
 		launch(args);
-	}
-
-	private static void loadProperties() throws IOException {
-		FileInputStream inStream = new FileInputStream(PROPERTIES_FILE);
-		options.load(inStream);
-		inStream.close();
 	}
 
 	@Override
--- src/main/java/com/serpest/rebuk/controller/MainAppController.java
+++ src/main/java/com/serpest/rebuk/controller/MainAppController.java
@@ -1,6 +1,10 @@
 package com.serpest.rebuk.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.Properties;
 
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -11,6 +15,23 @@ public class MainAppController {
 	public final static Image APP_ICON = new Image(MainAppController.class.getResource("/img/icon.png").toString());
 
 	public final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("d MMM yyyy HH:mm");
+
+	private static final String PROPERTIES_FILE = "data" + File.separator + "Rebuk_options.properties";
+
+	public static Properties options;
+
+	static {
+		try {
+			options = new Properties();
+			loadProperties();
+		} catch (IOException exc) {}
+	}
+
+	private static void loadProperties() throws IOException {
+		FileInputStream inStream = new FileInputStream(PROPERTIES_FILE);
+		options.load(inStream);
+		inStream.close();
+	}
 
 	Stage primaryStage;
 
